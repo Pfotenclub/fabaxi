aecd304288c08cd23b37a0ea6cc79be73a9ac885
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2024-04-26 17:36:21 +0200

    wd

--- bot.py
+++ bot.py
@@ -1,41 +1,30 @@
-print("Bot is starting...")
 from discord.ext import commands
 import discord
 import os # default module
 from dotenv import load_dotenv
-print("Bot is starting...")
 load_dotenv() # load all the variables from the env file
 bot = commands.Bot(debug_guilds=[1056514064081231872], intents=discord.Intents.all())
-print("Bot is ready!")
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
-
-
 if __name__ == '__main__':
     if os.path.isdir("commands"):
         os.chdir("commands")
-        print("Changed directory to commands")
     else:
         os.chdir("app/commands")
-        print("Changed directory to app/commands")
     for i in os.listdir():
         if i.endswith(".py"):
             try:
                 bot.load_extension(f"commands.{i[:-3]}")
-                print(f"{i} wurde geladen")
             except Exception as error:
                 print('{} konnte nicht geladen werden. [{}]'.format(i, error))
             else:
                 print(f"{i} wurde geladen")
     if os.path.isdir("./../events"):
-        print("Changed directory to events")
         os.chdir("./../events")
-        print("Changed directory to events")
     else:
         os.chdir("./../app/events")
-        print("Changed directory to app/events")
     for i in os.listdir():
         if i.endswith(".py"):
             try:
--- commands/stuff.py
+++ commands/stuff.py
@@ -8,19 +8,9 @@ class Stuff(commands.Cog): # create a class for our cog that inherits from comma
 
     def __init__(self, bot): # this is a special method that is called when the cog is loaded
         self.bot = bot
-    @discord.slash_command(name="pull", description="This is a test command", guild_ids=[1001916230069911703])
+    @discord.slash_command(name="ping", description="Pong!")
     async def pull(self, ctx):
-        # Pull code from GitHub repo
-
-        repo_url = "https://github.com/your_username/your_repo.git"
-        repo_dir = "/path/to/your/repo"
-
-        if not os.path.exists(repo_dir):
-            git.Repo.clone_from(repo_url, repo_dir)
-        else:
-            repo = git.Repo(repo_dir)
-            repo.remotes.origin.pull()
-        await ctx.send("This is a test command")
+        await ctx.respond("Pong!")
 
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(Stuff(bot)) # add the cog to the bot
