954f9965bb48177d0b418dcfd0481687c3cfcde4
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2024-04-27 07:59:03 +0200

    Update stuff.py

--- commands/stuff.py
+++ commands/stuff.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import os
-import git
 
 class Stuff(commands.Cog): # create a class for our cog that inherits from commands.Cog
     # this class is used to create a cog, which is a module that can be added to the bot
