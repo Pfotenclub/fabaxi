c76bb125942abe13ac9c1112bf09f6c3d43ec051
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2024-04-28 20:32:19 +0200

    added monitoring

---
+++ events/stuff.py
@@ -0,0 +1,17 @@
+from discord.ext import commands, tasks
+import requests
+
+class StatusPageUpdate(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.update_status.start()
+
+    def cog_unload(self):
+        self.update_status.cancel()
+
+    @tasks.loop(seconds=19.0)
+    async def update_status(self):
+        requests.get('https://klara.wolfiii.gay/api/push/FfkEDcIxAt?status=up&msg=OK&ping=')
+
+def setup(bot):
+    bot.add_cog(StatusPageUpdate(bot))
