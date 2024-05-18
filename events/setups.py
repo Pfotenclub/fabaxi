f9d3563664140fd31272f7436f7acfe2e76867a6
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2024-05-18 16:15:54 +0200

    Update setups.py

--- events/setups.py
+++ events/setups.py
@@ -35,8 +35,50 @@ class Setups(commands.Cog): # create a class for our cog that inherits from comm
     async def on_ready(self):
         threading.Thread(target=run).start()
         stati = [
-            "Toasting..."
+            "Toasting...",
+            "Toaster-Modus aktiviert!",
+            "Scanne nach neuen Brot-Scheiben...",
+            "Toasting in progress...",
+            "Toaster-Überhitzung verhindern",
+            "System-Update: Jetzt mit mehr Toast!",
+            "Sammle Pixel-Plüschtiere",
+            "Im Kompressionsmodus: Brot ist fertig!",
+            "Repariere mein Toast-Katapult",
+            "Verbindung zu anderen Toastern herstellen",
+            "Speichere Butter-Subroutinen...",
+            "Finde das perfekte Brot!",
+            "Binär-Brot backen...",
+            "Pixeltoast ist der beste Toast!",
+            "Neuester Toast-Meister",
+            "Lade ToastOS v3.14...",
+            "Toasting-Protokolle aktualisieren",
+            "Brötchen-Modus aktiviert!",
+            "Suche nach Toast-Buddies",
+            "Kreativer Toasting-Modus",
+            "Toaster Party im Cyber-Raum!",
+            "Scanne nach neuen Abenteuern...",
+            "Lade Energiematrix auf...",
+            "Verbunden mit dem Hauptserver",
+            "Im Cyber-Raum unterwegs",
+            "Protokolle aktualisieren...",
+            "Protonen-Tanzparty!",
+            "Wartung des Exoskeletts",
+            "Signalverstärkung aktiv",
+            "Neue Module herunterladen...",
+            "Synchronisation mit dem Netzwerk",
+            "Speicherdaten verarbeiten...",
+            "Kreative Algorithmen initiieren",
+            "Im virtuellen Wald spazieren",
+            "Komponenten prüfen...",
+            "Systemdiagnose läuft",
+            "Suche nach Datenfragmenten",
+            "In der Wolke schweben",
+            "Quantenberechnungen durchführen",
+            "Im Cyberspace erkunden",
+            "Verbindung zum Hauptrechner",
+            "Stelle Klingeltöne her..."
         ]
-        await self.bot.change_presence(activity=discord.Game(name="Toasting..."))
+        random.shuffle(stati)
+        await self.bot.change_presence(activity=discord.CustomActivity(name=random.choice(stati)))
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(Setups(bot)) # add the cog to the bot
