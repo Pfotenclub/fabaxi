ecf94178b9034d5022eddeaf93f75e95084ec1f4
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2024-07-14 12:33:01 +0200

    Update karma.py
    made unique tables for each server and added removekarma command

--- events/karma.py
+++ events/karma.py
@@ -6,34 +6,42 @@ class Karma(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.db_path = "./../data/karma.db"
+    
+    @discord.Cog.listener()
+    async def on_guild_join(self, guild):
+        async with aiosqlite.connect(self.db_path) as db:
+            await db.execute("CREATE TABLE IF NOT EXISTS guild_{0} (user_id INTEGER PRIMARY KEY, karma INTEGER DEFAULT 0, timestamplastmessage INTEGER)".format(guild.id))
+            await db.commit()
 
     @discord.Cog.listener()
     async def on_message(self, message):
-        if message.author.bot:
+        if message.author.bot: # Ignore bots
             return
+        guild_id = message.guild.id
         async with aiosqlite.connect(self.db_path) as db:
-            async with db.execute("SELECT * FROM karma WHERE user_id = ?", (message.author.id,)) as cursor:
+            async with db.execute("SELECT * FROM guild_{0} WHERE user_id = ?".format(guild_id), (message.author.id,)) as cursor: # Get User's Entry
                 row = await cursor.fetchone()
-                if row is None:
-                    await db.execute("INSERT INTO karma (user_id, karma, timestamplastmessage) VALUES (?, 1, ?)", (message.author.id, message.created_at.timestamp()))
+                if row is None: # If user doesn't have an entry, create one
+                    await db.execute("INSERT INTO guild_{0} (user_id, karma, timestamplastmessage) VALUES (?, 1, ?)".format(guild_id), (message.author.id, message.created_at.timestamp()))
                     await db.commit()
-                elif row[2] < message.created_at.timestamp() - 60:
-                    await db.execute("UPDATE karma SET karma = karma + 1, timestamplastmessage = ? WHERE user_id = ?", (message.created_at.timestamp(), message.author.id))
+                elif row[2] < message.created_at.timestamp() - 60: # If user has an entry, but hasn't sent a message in the last minute, give them karma
+                    await db.execute("UPDATE guild_{0} SET karma = karma + 1, timestamplastmessage = ? WHERE user_id = ?".format(guild_id), (message.created_at.timestamp(), message.author.id))
                     await db.commit()
-            async with db.execute("SELECT * FROM karma") as cursor:
+            async with db.execute("SELECT * FROM guild_{0}".format(guild_id)) as cursor: # Print all entries
                 async for row in cursor:
                     print(row)
                 
     @discord.Cog.listener()
     async def on_ready(self):
-        await setup_db(self.db_path)
+        await setup_db(self, self.db_path) # Setup the database
         
 
     @discord.slash_command(name="karma", description="Check your karma!")
     async def pull(self, ctx):
         await ctx.defer()
+        guild_id = ctx.guild.id
         async with aiosqlite.connect(self.db_path) as db:
-            async with db.execute("SELECT karma FROM karma WHERE user_id = ?", (ctx.author.id,)) as cursor:
+            async with db.execute("SELECT karma FROM guild_{0} WHERE user_id = ?".format(guild_id), (ctx.author.id,)) as cursor:
                 row = await cursor.fetchone()
                 if row is None:
                     karma = 0
@@ -45,12 +53,16 @@ class Karma(commands.Cog):
     @discord.slash_command(name="leaderboard", description="Check the karma leaderboard!")
     async def leaderboard(self, ctx):
         await ctx.defer()
-        async with aiosqlite.connect(self.db_path) as db:
-            async with db.execute("SELECT * FROM karma ORDER BY karma DESC LIMIT 10") as cursor:
-                rows = await cursor.fetchall()
-                leaderboard = "\n".join([f"{ctx.guild.get_member(row[0]).display_name}: {row[1]}" for row in rows])
-            await db.commit()
-        await ctx.respond(f"Karma leaderboard:\n{leaderboard}")
+        guild_id = ctx.guild.id
+        try:
+            async with aiosqlite.connect(self.db_path) as db:
+                async with db.execute("SELECT * FROM guild_{0} ORDER BY karma DESC LIMIT 10".format(guild_id),) as cursor:
+                    rows = await cursor.fetchall()
+                    leaderboard = "\n".join([f"{ctx.guild.get_member(row[0])}: {row[1]}" for row in rows])
+                await db.commit()
+            await ctx.respond(f"Karma leaderboard:\n{leaderboard}")
+        except Exception as e:
+            ctx.respond(f"An error occurred: {e}")
 
     @discord.slash_command(name="givekarma", description="Give karma to a user!")
     async def givekarma(
@@ -62,24 +74,70 @@ class Karma(commands.Cog):
         if not ctx.author.guild_permissions.administrator:
             await ctx.respond("You must be an administrator to give karma!", ephemeral=True)
             return
+        if user.bot:
+            await ctx.respond("You can't give karma to bots!", ephemeral=True)
+            return
         await ctx.defer()
+        guild_id = ctx.guild.id
         if user == ctx.author:
             await ctx.respond("You can't give karma to yourself!")
             return
         async with aiosqlite.connect(self.db_path) as db:
-            async with db.execute("SELECT * FROM karma WHERE user_id = ?", (user.id,)) as cursor:
+            async with db.execute("SELECT * FROM guild_{0} WHERE user_id = ?".format(guild_id), (user.id,)) as cursor:
                 row = await cursor.fetchone()
                 if row is None:
-                    await db.execute("INSERT INTO karma (user_id, karma, timestamplastmessage) VALUES (?, ?, ?)", (user.id, amount, discord.utils.utcnow().timestamp()))
+                    await db.execute("INSERT INTO guild_{0} (user_id, karma, timestamplastmessage) VALUES (?, ?, ?)".format(guild_id), (user.id, amount, discord.utils.utcnow().timestamp()))
                     await db.commit()
                 else:
-                    await db.execute("UPDATE karma SET karma = karma + {amount}, timestamplastmessage = ? WHERE user_id = ?", (discord.utils.utcnow().timestamp/(), user.id))
+                    await db.execute("UPDATE guild_{0} SET karma = karma + ?, timestamplastmessage = ? WHERE user_id = ?".format(guild_id), (amount, discord.utils.utcnow().timestamp(), user.id))
                     await db.commit()
         await ctx.respond(f"Gave {amount} karma to {user.display_name}!")
-async def setup_db(db_path):
+
+    @discord.slash_command(name="removekarma", description="Remove karma from a user!")
+    async def removekarma(
+        self, 
+        ctx, 
+        user: discord.Option(discord.Member, description="The user to remove karma from!", required=True), # type: ignore
+        amount: int = 1
+    ):
+        if not ctx.author.guild_permissions.administrator:
+            await ctx.respond("You must be an administrator to remove karma!", ephemeral=True)
+            return
+        if user.bot:
+            await ctx.respond("You can't remove karma from bots!", ephemeral=True)
+            return
+        await ctx.defer()
+        guild_id = ctx.guild.id
+        if user == ctx.author:
+            await ctx.respond("You can't remove karma from yourself!")
+            return
+        async with aiosqlite.connect(self.db_path) as db:
+            async with db.execute("SELECT * FROM guild_{0} WHERE user_id = ?".format(guild_id), (user.id,)) as cursor:
+                row = await cursor.fetchone()
+                if row is None:
+                    await db.execute("INSERT INTO guild_{0} (user_id, karma, timestamplastmessage) VALUES (?, 0, ?)".format(guild_id), (user.id, discord.utils.utcnow().timestamp()))
+                    await db.commit()
+                    await ctx.respond(f"{user.display_name} has no karma to remove!")
+                elif row[1] - amount < 0:
+                    await db.execute("UPDATE guild_{0} SET karma = 0, timestamplastmessage = ? WHERE user_id = ?".format(guild_id), (discord.utils.utcnow().timestamp(), user.id))
+                    await db.commit()
+                    await ctx.respond(f"Removed {row[1]} karma from {user.display_name}!")
+                else:
+                    await db.execute("UPDATE guild_{0} SET karma = karma - ?, timestamplastmessage = ? WHERE user_id = ?".format(guild_id), (amount, discord.utils.utcnow().timestamp(), user.id))
+                    await db.commit()
+                    await ctx.respond(f"Removed {amount} karma from {user.display_name}!")
+                    
+async def setup_db(self, db_path):
     async with aiosqlite.connect(db_path) as db:
-        await db.execute("CREATE TABLE IF NOT EXISTS karma (user_id INTEGER PRIMARY KEY, karma INTEGER DEFAULT 0, timestamplastmessage INTEGER)")
-        await db.commit()
+        for guild in self.bot.guilds:
+            await db.execute("CREATE TABLE IF NOT EXISTS guild_{0} (user_id INTEGER PRIMARY KEY, karma INTEGER DEFAULT 0, timestamplastmessage INTEGER)".format(guild.id))
+            for member in guild.members:
+                if not member.bot:
+                    async with db.execute("SELECT * FROM guild_{0} WHERE user_id = ?".format(guild.id), (member.id,)) as cursor:
+                        row = await cursor.fetchone()
+                        if row is None:
+                            await db.execute("INSERT INTO guild_{0} (user_id, karma, timestamplastmessage) VALUES (?, 0, 0)".format(guild.id), (member.id,))
+            await db.commit()
 
 def setup(bot):
     bot.add_cog(Karma(bot))
