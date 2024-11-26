f182fe362ea58bc4c754ac4f7d3d2ded7fe2d239
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2024-11-26 20:08:27 +0100

    Update nightclub.py

--- commands/nightclub.py
+++ commands/nightclub.py
@@ -6,11 +6,14 @@ class NightclubInterface(discord.ui.Button):
     def __init__(self):
         super().__init__(
             style=discord.ButtonStyle.primary,
-            label="Approve Me!"
+            label="Approve Me!",
+            custom_id="approve_button"
             )
 
     async def callback(self, interaction: discord.Interaction):
-        await interaction.response.send_message("Welcome to the nightclub!", ephemeral=True)
+        if interaction.user.id != 327880195476422656:
+            return await interaction.response.send_message("You are not the owner of the bot!", ephemeral=True)
+        
 
 class Nightclub(commands.Cog): # create a class for our cog that inherits from commands.Cog
     # this class is used to create a cog, which is a module that can be added to the bot
@@ -21,7 +24,7 @@ class Nightclub(commands.Cog): # create a class for our cog that inherits from c
     async def nightclub(self, ctx):
         if ctx.author.id != 327880195476422656:
             return await ctx.respond("You are not the owner of the bot!", ephemeral=True)
-        view = discord.ui.View()
+        view = discord.ui.View(timeout=None)
         view.add_item(NightclubInterface())
 
         embed = discord.Embed(
@@ -29,6 +32,20 @@ class Nightclub(commands.Cog): # create a class for our cog that inherits from c
             description="Click the button to get approved to join the Nightclub!\r\nPlease note that this will start a manual verification process. Only click on this button if you are 18+.",
             color=discord.Color.blurple()
             )
+        await ctx.send(embed=embed, view=view)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        view = discord.ui.View(timeout=None)
+        view.add_item(NightclubInterface())
+        self.bot.add_view(view)
+
+class NightclubAgeVerification(discord.ui.Modal):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(
+            discord.ui.
+        )
+
 
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(Nightclub(bot)) # add the cog to the bot
