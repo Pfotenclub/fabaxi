0673a8ebfdac5e534e5ff3d47e0c022d66c6647f
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2024-11-26 20:46:56 +0100

    Update nightclub.py

--- commands/nightclub.py
+++ commands/nightclub.py
@@ -14,6 +14,8 @@ class NightclubInterface(discord.ui.Button):
         if interaction.user.id != 327880195476422656:
             return await interaction.response.send_message("You are not the owner of the bot!", ephemeral=True)
         
+        await interaction.response.send_modal(NightclubAgeVerification(title="Age Verification"))
+        
 
 class Nightclub(commands.Cog): # create a class for our cog that inherits from commands.Cog
     # this class is used to create a cog, which is a module that can be added to the bot
@@ -43,8 +45,30 @@ class Nightclub(commands.Cog): # create a class for our cog that inherits from c
 class NightclubAgeVerification(discord.ui.Modal):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(
-            discord.ui.
+            discord.ui.InputText(
+                label="Enter your age",
+                placeholder="Your Age",
+                max_length=2,
+            ),
+            *args,
+            **kwargs
         )
+    async def callback(self, interaction: discord.Interaction):
+        if interaction.user.id != 327880195476422656:
+            return await interaction.respond("You are not the owner of the bot!", ephemeral=True)
+        if interaction.user.get_role(1229064333993050123) is not None:
+            await interaction.guild.get_channel(1283842433284837396).send(f"{interaction.user.mention} has requested to join the Nightclub with the <18 Role...")
+            return await interaction.respond("You have the <18 Role...", ephemeral=True)
+        if not self.children[0].value:
+            return await interaction.response.send_message("You must enter your age!", ephemeral=True)
+        elif not self.children[0].value.isnumeric():
+            return await interaction.response.send_message("You must enter a valid number!", ephemeral=True)
+        elif int(self.children[0].value) < 18:
+            await interaction.guild.get_channel(1283842433284837396).send(f"{interaction.user.mention} has requested to join the Nightclub with an **underage** of {self.children[0].value} years...")
+            return await interaction.respond("Only members 18 years old or older can get approved for the Nightclub!", ephemeral=True)
+        await interaction.guild.get_channel(1283842433284837396).send(f"{interaction.user.mention} has requested to join the Nightclub with an age of {self.children[0].value} years...")
+        await interaction.respond("Your request to the Nightclub has been forwarded to the Serverteam!", ephemeral=True)
+
 
 
 def setup(bot): # this is called by Pycord to setup the cog
