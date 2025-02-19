520a4662db65942d7802fbfeac576119900c2bfd
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2025-02-19 21:58:40 +0100

    Update utils.py

--- events/utils.py
+++ events/utils.py
@@ -11,7 +11,7 @@ class Utils(commands.Cog): # create a class for our cog that inherits from comma
     @discord.Cog.listener("on_raw_reaction_add")
     async def chooseRoleColor(self, payload):
         if payload.member.bot: return
-        if payload.message_id != 1341874136795971726: return
+        if payload.message_id != 1341876396611604581: return
         rolecolors = {
                 1341767688384675861: "🍏",
                 1341767941586423930: "🍎",
