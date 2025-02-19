b508c657df3f4475b53aea59aa98806e0dfc43a5
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2025-02-19 15:41:21 +0100

    Update stuff.py

--- commands/stuff.py
+++ commands/stuff.py
@@ -10,6 +10,16 @@ class Stuff(commands.Cog): # create a class for our cog that inherits from comma
     @discord.slash_command(name="ping", description="Pong!")
     async def pull(self, ctx):
         await ctx.respond("Pong!")
+    @discord.slash_command(name="stuff", description="Some stuff")
+    async def stuff(self, ctx):
+        if ctx.author.id != 327880195476422656: return await ctx.respond("You are not allowed to use this command")
+        await ctx.defer()
+        for member in ctx.guild.members:
+            if member.bot:
+                continue
+            await member.add_roles(ctx.guild.get_role(1341774758076874832))
+            print(f"Gave role to {member.name}")
+        await ctx.send("Done")
 
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(Stuff(bot)) # add the cog to the bot
