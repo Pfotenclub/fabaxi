abe8671921520f2315b4a818f8ce21fbd0a6c2a5
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2025-02-22 14:31:40 +0100

    Update stuff.py

--- commands/stuff.py
+++ commands/stuff.py
@@ -18,7 +18,7 @@ class Stuff(commands.Cog): # create a class for our cog that inherits from comma
     @tasks.loop(hours=24)
     async def check_birthdays(self):
         now = datetime.now()
-        target_time = time(14, 5)
+        target_time = time(4, 0)
         target_datetime = datetime.combine(now.date(), target_time)
 
         if now > target_datetime:
@@ -47,7 +47,7 @@ class Stuff(commands.Cog): # create a class for our cog that inherits from comma
     @tasks.loop(hours=24)
     async def remove_birthday_role(self):
         now = datetime.now()
-        target_time = time(14, 8)
+        target_time = time(0, 0)
         target_datetime = datetime.combine(now.date(), target_time)
 
         if now > target_datetime:
