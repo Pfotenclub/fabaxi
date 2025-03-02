608f0d998a455e2b34f3ed68d919aab6b63d4651
MuffinAmor <niko@muffinamor.dev>
2025-03-02 21:16:47 +0100

    db optimized,

--- db/temp_voice.py
+++ db/temp_voice.py
@@ -5,8 +5,9 @@ from sqlalchemy import insert, select, delete, update
 from db import Database
 from db.tables import TempVoiceTable
 
+
 class TempVoiceBackend(Database):
-    
+
     async def create_temp_voice(self, owner_id, channel_id, guild_id):
         async with self.get_session() as session:
             existing_record = await session.execute(
@@ -26,21 +27,22 @@ class TempVoiceBackend(Database):
             await session.execute(delete_stmt)
             await session.commit()
             logging.info(f"Deleted temp voice channel for channel_id={channel_id}")
-    
+
     async def get_owner_id(self, channel_id):
         async with self.get_session() as session:
             record = await session.execute(
                 select(TempVoiceTable.owner_id).where(TempVoiceTable.channel_id == channel_id))
             return record.scalars().first()
-    
+
     async def get_channel_id(self, owner_id):
         async with self.get_session() as session:
-            record = await session.execute(
-                select(TempVoiceTable.channel_id).where(TempVoiceTable.owner_id == owner_id))
+            record = await session.execute(select(TempVoiceTable.channel_id).where(TempVoiceTable.owner_id == owner_id))
             return record.scalars().first()
+
     async def change_channel_owner_id(self, channel_id, new_owner_id):
         async with self.get_session() as session:
-            update_stmt = update(TempVoiceTable).where(TempVoiceTable.channel_id == channel_id).values(owner_id=new_owner_id)
+            update_stmt = update(TempVoiceTable).where(TempVoiceTable.channel_id == channel_id).values(
+                owner_id=new_owner_id)
             await session.execute(update_stmt)
             await session.commit()
             logging.info(f"Changed owner_id for channel_id={channel_id} to new_owner_id={new_owner_id}")
