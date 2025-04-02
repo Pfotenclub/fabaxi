fba7ed965195947c17a55ae9b7e61ee326481742
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2025-04-02 13:51:47 +0200

    added the default embed generator where neccessary

--- app/commands/fun.py
+++ app/commands/fun.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 import random
+from ext.system import default_embed
 
 class Fun(commands.Cog): # create a class for our cog that inherits from commands.Cog
     # this class is used to create a cog, which is a module that can be added to the bot
@@ -86,7 +87,7 @@ class Fun(commands.Cog): # create a class for our cog that inherits from command
             response = random.choice(funnyResponses)
         else:
             response = random.choice(responses)
-        embed = discord.Embed()
+        embed: discord.Embed = await default_embed(ctx.author)
         embed.title = "Magic 8ball"
         embed.set_thumbnail(url="https://cdn.pixabay.com/photo/2015/09/05/07/17/pool-ball-923833_960_720.png")
         embed.add_field(name="Question", value=question, inline=False)
