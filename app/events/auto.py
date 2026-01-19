57b67b24f8e5b3e36a42abc58d78d38b7d159693
DiniskaWolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2026-01-19 23:28:34 +0100

    Refactor error handling in AutoCommands to improve logging and user feedback; add alert parameter for system messages

--- app/events/auto.py
+++ app/events/auto.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 import discord
 from discord.ext import commands
+from ext.system import send_system_message
 
 
 class AutoCommands(commands.Cog):
@@ -12,14 +13,23 @@ class AutoCommands(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.CommandNotFound):
-            if ctx.message.content == "n!help":
-                await ctx.send("Bitte beachte, dass wir nun / Commands benutzen.")
-        elif isinstance(error, commands.CheckFailure):
+        if isinstance(error, commands.CheckFailure):
             await ctx.send("You are not authorized to use this command.")
+            send_system_message(
+                picture_url=self.bot.user.avatar,
+                username=self.bot.user.name,
+                content=f"Unauthorized access attempt by {ctx.author} ({ctx.author.id}) ({ctx.guild.id}) for command `{ctx.command}`.",
+            )
         else:
             self.counter = + 1
             # channel = self.bot.get_channel(data["error"])
+            send_system_message(
+                picture_url=self.bot.user.avatar,
+                username=self.bot.user.name,
+                content=f"Error report Nr. {self.counter} after reset.\nServer: {ctx.message.guild}\nCommand: {ctx.message.content}\nError: {error}",
+                alert=True
+            )
+            """ Storing this for archive purposes, maybe useful later
             embed = discord.Embed(title="Ops, there is an error!",
                                   description="Error report Nr. {} after reset.".format(self.counter),
                                   color=ctx.author.color)
@@ -32,13 +42,22 @@ class AutoCommands(commands.Cog):
             await ctx.channel.send(embed=embed)
             # await channel.send(embed=embed)
             print(error)
+            """
 
     @commands.Cog.listener()
     async def on_application_command_error(self, ctx, error):
         print(error)
         if str(error).startswith("The check functions"):
-            await ctx.respond("Es scheint als seist du nicht registriert")
+            await ctx.respond("You are not authorized to use this command.")
         else:
+            self.counter = + 1
+            send_system_message(
+                picture_url=self.bot.user.avatar,
+                username=self.bot.user.name,
+                content=f"Error report Nr. {self.counter} after reset.\nServer: {ctx.guild}\nCommand: {ctx.command.name}\nError: {error}",
+                alert=True
+            )
+            """ Storing this for archive purposes, maybe useful later
             # channel = self.bot.get_channel(data["error"])
             embed = discord.Embed(title="Ops, there is an error!",
                                   color=ctx.author.color)
@@ -50,6 +69,7 @@ class AutoCommands(commands.Cog):
             embed.timestamp = datetime.utcnow()
             await ctx.respond(embed=embed)
             # await channel.send(embed=embed)
+            """
 
 def setup(bot):
     bot.add_cog(AutoCommands(bot))
